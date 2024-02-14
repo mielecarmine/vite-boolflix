a34d2e34b714af6bff0489eae6fd97edfe42763e
mielecarmine <mielecarmine993@gmail.com>
2024-02-14 17:37:19 +0100

    add: axios package

--- src/main.js
+++ src/main.js
@@ -11,10 +11,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { all } from "@awesome.me/kit-KIT_CODE";
+// import { all } from "@awesome.me/kit-KIT_CODE";
 
 /* add icons to the library */
-library.add(...all);
+// library.add(...all);
 
 const app = createApp(App);
 app.mount("#app");
