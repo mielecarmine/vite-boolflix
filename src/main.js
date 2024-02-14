93d13282b22935777261ef0383d791efb241a264
mielecarmine <mielecarmine993@gmail.com>
2024-02-14 17:22:38 +0100

    add: boostrap and fontawesome packages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// import boostrapp css
+import "bootstrap/dist/css/bootstrap.css";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { all } from "@awesome.me/kit-KIT_CODE";
+
+/* add icons to the library */
+library.add(...all);
+
+const app = createApp(App);
+app.mount("#app");
+app.component("font-awesome-icon", FontAwesomeIcon);
