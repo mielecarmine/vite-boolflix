f8be89c5a95a57072374a905100f5d25ec8ffb7f
mielecarmine <mielecarmine993@gmail.com>
2024-02-15 12:55:30 +0100

    feat: flags

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,24 @@ export default {
           `${store.api.uri}/search/movie?query=${this.input_text}&language=it-IT&api_key=${store.api.key}`
         )
         .then((res) => {
-          store.films = res.data.results;
+          store.films = res.data.results.map((film) => {
+            return {
+              title: film.title,
+              original_title: film.original_title,
+              original_language: film.original_language,
+              vote_avrg: Math.floor(film.vote_average),
+            };
+          });
           console.log(store.films);
         });
+    },
+
+    getFlag(langCode) {
+      if (langCode == "en")
+        return new URL("./assets/img/eng.jpg", import.meta.url).href;
+      if (langCode == "it")
+        return new URL("./assets/img/it.png", import.meta.url).href;
+      return "bandiera default";
     },
   },
 };
@@ -41,21 +56,22 @@ export default {
       />
       <button @click="getFilms()" class="btn btn-primary">Cerca</button>
     </div>
-    <ul v-for="(film, index) in store.films" class="list-group">
+    <ul v-for="(film, index) in store.films" class="list-group pb-3">
       <li class="list-group-item"><b>Titolo:</b> {{ film.title }}</li>
       <li class="list-group-item">
         <b>Titolo originale:</b> {{ film.original_title }}
       </li>
       <li class="list-group-item">
-        <b>Lingua originale:</b> {{ film.original_language }}
+        <img :src="getFlag(film.original_language)" alt="" />
       </li>
-      <li class="list-group-item">
-        <b>Voto medio:</b> {{ film.vote_average }}
-      </li>
+      <li class="list-group-item"><b>Voto medio:</b> {{ film.vote_avrg }}</li>
     </ul>
   </div>
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss";
+img {
+  width: 50px;
+}
 </style>
